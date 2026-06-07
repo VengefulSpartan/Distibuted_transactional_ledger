54e6a0f9b64be79485d3e873c6f9c4b6ddfece08
Shakti <shaktisahoo24@gmail.com>
2026-06-07 13:30:51 +0530

    dockerd up

--- main.py
+++ main.py
@@ -14,24 +14,24 @@ def start_node(node_id):
     # -------------------------------------------------
     # Map Node IDs to Ports
     registry = {
-        1: 5001,
-        2: 5002,
-        3: 5003,
-        4: 5004
+        1: ('node1',5001),
+        2: ('node2',5002),
+        3: ('node3',5003),
+        4: ('node4',5004)
     }
 
     if node_id not in registry:
         print(f"Error: Node ID {node_id} not found in registry.")
         return
 
-    my_port = registry[node_id]
-    my_address = ('localhost', my_port)
+    my_host , my_port = registry[node_id]
+    my_address = ('0.0.0.0', my_port)
 
     # Calculate Peers (Everyone who is NOT me)
     peers = []
-    for n_id, p_port in registry.items():
+    for n_id, (p_host,p_port) in registry.items():
         if n_id != node_id:
-            peers.append(('localhost', p_port))
+            peers.append((p_host, p_port))
 
     print(f"[*] Node {node_id} starting on {my_address}")
     print(f"[*] Peers: {peers}")
